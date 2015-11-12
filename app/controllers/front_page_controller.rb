0cae69f837e7935594f16ade678feb097b7eab5d
deartrudence <trudy.macnabb@gmail.com>
2015-11-12 09:38:23 -0500

    small changes

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,6 +1,6 @@
 class FrontPageController < ApplicationController
     skip_before_action :authenticate_user!
 	def show
-		@portfolio_pieces = PortfolioPiece.all.order(:order)		
+		@portfolio_pieces = PortfolioPiece.all.order(:order).limit(3)		
 	end
 end
